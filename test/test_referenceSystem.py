64d57dfb0353f55cae4360d37bae2d1d7a81b193
Jon Blower <j.d.blower@reading.ac.uk>
2021-11-14 17:55:38 +0000

    Added schema and tests for reference systems

---
+++ test/test_referenceSystem.py
@@ -0,0 +1,163 @@
+# Pytests to test the referenceSystem.json schema file
+
+# TODO: test that "calendar" is "Gregorian" or a URI
+
+import pytest
+from jsonschema.exceptions import ValidationError
+
+import validator
+
+VALIDATOR = validator.create_custom_validator("/schemas/referenceSystem")
+
+
+def test_geographic_rs():
+    ''' Tests a minimal geographic RS '''
+
+    crs = {
+        "type" : "GeographicCRS",
+        "id" : "http://www.opengis.net/def/crs/OGC/1.3/CRS84"
+    }
+    VALIDATOR.validate(crs)
+
+
+def test_projected_rs():
+    ''' Tests a minimal projected RS '''
+
+    crs = {
+        "type" : "ProjectedCRS",
+        "id" : "http://www.opengis.net/def/crs/EPSG/0/27700"
+    }
+    VALIDATOR.validate(crs)
+
+
+def test_vertical_rs():
+    ''' Tests a minimal vertical RS '''
+
+    crs = {
+        "type" : "VerticalCRS",
+        "id" : "http://www.opengis.net/def/crs/EPSG/0/5703"
+    }
+    VALIDATOR.validate(crs)
+
+
+def test_minimal_temporal_rs():
+    ''' Tests a minimal temporal RS '''
+
+    crs = {
+        "type" : "TemporalRS",
+        "calendar" : "Gregorian"
+    }
+    VALIDATOR.validate(crs)
+
+
+def test_identifier_rs():
+    ''' Tests an example of an IdentifierRS '''
+
+    crs = {
+        "type": "IdentifierRS",
+        "id": "https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2",
+        "label": { "en": "ISO 3166-1 alpha-2 codes" },
+        "targetConcept": {
+            "id": "http://dbpedia.org/resource/Country",
+            "label": {"en": "Country", "de": "Land" }
+        },
+        "identifiers": {
+            "de": {
+                "id": "http://dbpedia.org/resource/Germany",
+                "label": { "de": "Deutschland", "en": "Germany" }
+            },
+            "gb": {
+                "id": "http://dbpedia.org/resource/United_Kingdom",
+                "label": { "de": "Vereinigtes Königreich", "en": "United Kingdom" }
+            }
+        }
+    }
+    VALIDATOR.validate(crs)
+
+
+def test_missing_type():
+    ''' Tests an RS with a missing type '''
+
+    crs = { "id" : "http://www.opengis.net/def/crs/OGC/1.3/CRS84" }
+    with pytest.raises(ValidationError):
+        VALIDATOR.validate(crs)
+
+
+def test_missing_calendar():
+    ''' Tests a TemporalRS with a missing calendar '''
+
+    crs = { "type" : "TemporalRS" }
+    with pytest.raises(ValidationError):
+        VALIDATOR.validate(crs)
+
+
+def test_missing_target_concept():
+    ''' Tests an IdentifierRS with a missing targetConcept '''
+
+    crs = {
+        "type": "IdentifierRS",
+        "id": "https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2",
+        "label": { "en": "ISO 3166-1 alpha-2 codes" },
+        "identifiers": {
+            "de": {
+                "id": "http://dbpedia.org/resource/Germany",
+                "label": { "de": "Deutschland", "en": "Germany" }
+            },
+            "gb": {
+                "id": "http://dbpedia.org/resource/United_Kingdom",
+                "label": { "de": "Vereinigtes Königreich", "en": "United Kingdom" }
+            }
+        }
+    }
+    with pytest.raises(ValidationError):
+        VALIDATOR.validate(crs)
+
+
+def test_invalid_target_concept():
+    ''' Tests an IdentifierRS with an invalid targetConcept (missing label) '''
+
+    crs = {
+        "type": "IdentifierRS",
+        "id": "https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2",
+        "label": { "en": "ISO 3166-1 alpha-2 codes" },
+        "targetConcept": {
+            "id": "http://dbpedia.org/resource/Country"
+        },
+        "identifiers": {
+            "de": {
+                "id": "http://dbpedia.org/resource/Germany",
+                "label": { "de": "Deutschland", "en": "Germany" }
+            },
+            "gb": {
+                "id": "http://dbpedia.org/resource/United_Kingdom",
+                "label": { "de": "Vereinigtes Königreich", "en": "United Kingdom" }
+            }
+        }
+    }
+    with pytest.raises(ValidationError):
+        VALIDATOR.validate(crs)
+
+
+def test_invalid_identifier():
+    ''' Tests an IdentifierRS with an invalid identifier (missing label) '''
+
+    crs = {
+        "type": "IdentifierRS",
+        "id": "https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2",
+        "label": { "en": "ISO 3166-1 alpha-2 codes" },
+        "targetConcept": {
+            "id": "http://dbpedia.org/resource/Country",
+            "label": {"en": "Country", "de": "Land" }
+        },
+        "identifiers": {
+            "de": {
+                "id": "http://dbpedia.org/resource/Germany"
+            },
+            "gb": {
+                "id": "http://dbpedia.org/resource/United_Kingdom",
+                "label": { "de": "Vereinigtes Königreich", "en": "United Kingdom" }
+            }
+        }
+    }
+    with pytest.raises(ValidationError):
+        VALIDATOR.validate(crs)
