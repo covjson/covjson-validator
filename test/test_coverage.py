a6fe4c77a2fdccdd12e265be14606735068f16a3
Maik Riechert <maik.riechert@arcor.de>
2022-02-02 17:17:47 +0000

    Add Coverage[Collection] schemas and tests (#17)
    * add TiledNdArray schema
    
    * schemas & tests for Coverage(Collection)
    
    * Whitespace changes for flake8
    
    Co-authored-by: Jon Blower <j.d.blower@reading.ac.uk>

---
+++ test/test_coverage.py
@@ -0,0 +1,213 @@
+# Pytests to test the coverage.json schema file
+
+import pytest
+from jsonschema.exceptions import ValidationError
+
+import validator
+
+VALIDATOR = validator.create_custom_validator("/schemas/coverage")
+
+
+def get_sample_coverage():
+    ''' Returns a sample of a valid coverage '''
+
+    return {
+        "type" : "Coverage",
+        "domain" : {
+            "type" : "Domain",
+            "domainType" : "Grid",
+            "axes": {
+                "x" : { "values": [-10, -5, 0] },
+                "y" : { "values": [40, 50] },
+                "z" : { "values": [5] },
+                "t" : { "values": ["2010-01-01T00:12:20Z"] }
+            },
+            "referencing": [{
+                "coordinates": ["y", "x", "z"],
+                "system": {
+                    "type": "GeographicCRS",
+                    "id": "http://www.opengis.net/def/crs/EPSG/0/4979"
+                }
+            }, {
+                "coordinates": ["t"],
+                "system": {
+                    "type": "TemporalRS",
+                    "calendar": "Gregorian"
+                }
+            }]
+        },
+        "parameters" : {
+            "ICEC": {
+                "type" : "Parameter",
+                "observedProperty" : {
+                    "id" : "http://vocab.nerc.ac.uk/standard_name/sea_ice_area_fraction/",
+                    "label" : {
+                        "en": "Sea Ice Concentration"
+                    }
+                }
+            }
+        },
+        "ranges" : {
+            "ICEC" : {
+                "type" : "NdArray",
+                "dataType": "float",
+                "axisNames": ["t", "z", "y", "x"],
+                "shape": [1, 1, 2, 3],
+                "values" : [ 0.5, 0.6, 0.4, 0.6, 0.2, None ]
+            }
+        }
+    }
+
+
+def test_valid_coverage():
+    ''' Valid: Tests an example of a coverage '''
+
+    coverage = get_sample_coverage()
+    VALIDATOR.validate(coverage)
+
+
+def test_domain_is_url():
+    ''' Valid: Coverage with "domain" as a URL '''
+
+    coverage = get_sample_coverage()
+    coverage["domain"] = "http://example.com/domain.json"
+    VALIDATOR.validate(coverage)
+
+
+def test_range_is_url():
+    ''' Valid: Coverage with range as a URL '''
+
+    coverage = get_sample_coverage()
+    coverage["ranges"]["ICEC"] = "http://example.com/icec.json"
+    VALIDATOR.validate(coverage)
+
+
+def test_range_is_tiled():
+    ''' Valid: Coverage with tiled range '''
+
+    coverage = get_sample_coverage()
+    coverage["ranges"]["ICEC"] = {
+        "type" : "TiledNdArray",
+        "dataType": "float",
+        "axisNames": ["t", "z", "y", "x"],
+        "shape": [1, 1, 2, 3],
+        "tileSets": [{
+            "tileShape": [1, None, None, None],
+            "urlTemplate": "http://example.com/{t}.covjson"
+        }]
+    }
+    VALIDATOR.validate(coverage)
+
+
+def test_missing_type():
+    ''' Invalid: Coverage with missing "type" '''
+
+    coverage = get_sample_coverage()
+    del coverage["type"]
+    with pytest.raises(ValidationError):
+        VALIDATOR.validate(coverage)
+
+
+def test_misspelled_type():
+    ''' Invalid: Coverage with misspelled "type" '''
+
+    coverage = get_sample_coverage()
+    coverage["type"] = "Coverag"
+    with pytest.raises(ValidationError):
+        VALIDATOR.validate(coverage)
+
+
+def test_incorrect_id_type():
+    ''' Invalid: Coverage with incorrect "id" type '''
+
+    coverage = get_sample_coverage()
+    coverage["id"] = 42
+    with pytest.raises(ValidationError):
+        VALIDATOR.validate(coverage)
+
+
+def test_missing_domain():
+    ''' Invalid: Coverage with missing "domain" '''
+
+    coverage = get_sample_coverage()
+    del coverage["domain"]
+    with pytest.raises(ValidationError):
+        VALIDATOR.validate(coverage)
+
+
+def test_incorrect_domain_type():
+    ''' Invalid: Coverage with incorrect "domain" type '''
+
+    coverage = get_sample_coverage()
+    coverage["domain"]["type"] = "NotADomain"
+    with pytest.raises(ValidationError):
+        VALIDATOR.validate(coverage)
+
+
+def test_incorrect_domain_type_type():
+    ''' Invalid: Coverage with incorrect "domainType" type '''
+
+    coverage = get_sample_coverage()
+    coverage["domainType"] = [ "Grid" ]
+    with pytest.raises(ValidationError):
+        VALIDATOR.validate(coverage)
+
+
+def test_missing_parameters():
+    ''' Invalid: Coverage with missing "parameters" '''
+
+    coverage = get_sample_coverage()
+    del coverage["parameters"]
+    with pytest.raises(ValidationError):
+        VALIDATOR.validate(coverage)
+
+
+def test_incorrect_parameters_type():
+    ''' Invalid: Coverage with incorrect "parameters" type '''
+
+    coverage = get_sample_coverage()
+    coverage["parameters"] = list(coverage["parameters"].values())
+    with pytest.raises(ValidationError):
+        VALIDATOR.validate(coverage)
+
+
+def test_incorrect_parameter_groups_type():
+    ''' Invalid: Coverage with incorrect "parameterGroups" type '''
+
+    coverage = get_sample_coverage()
+    coverage["parameterGroups"] = coverage["parameters"]
+    with pytest.raises(ValidationError):
+        VALIDATOR.validate(coverage)
+
+
+def test_missing_ranges():
+    ''' Invalid: Coverage with missing "ranges" '''
+
+    coverage = get_sample_coverage()
+    del coverage["ranges"]
+    with pytest.raises(ValidationError):
+        VALIDATOR.validate(coverage)
+
+
+def test_incorrect_range_type():
+    ''' Invalid: Coverage with incorrect range type '''
+
+    coverage = get_sample_coverage()
+    coverage["ranges"]["ICEC"]["type"] = "SomethingElse"
+    with pytest.raises(ValidationError):
+        VALIDATOR.validate(coverage)
+
+
+def test_incorrect_alternate_ranges():
+    ''' Invalid: Coverage with incorrect "rangeAlternates" type '''
+
+    coverage = get_sample_coverage()
+    coverage["rangeAlternates"] = []
+    with pytest.raises(ValidationError):
+        VALIDATOR.validate(coverage)
+
+
+# TODO test that "ranges" keys match "parameters" keys
+# TODO test that "shape" of (Tiled)NdArray "ranges" matches "axes" of "domain"
+# TODO test that "values" of (Tiled)NdArray "ranges" matches the parameter's
+#      "categoryEncoding", if existing
