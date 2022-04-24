5a8cb7a67ed3e8b0c4fad27d657006dcfead6b85
Jon Blower <j.d.blower@reading.ac.uk>
2022-04-24 21:53:05 +0100

    Added playground JSON files and test routine

---
+++ test/test_data/playground/README.txt
@@ -0,0 +1,1 @@
+These are complete CoverageJSON objects, copied from the CoverageJSON playground. They are included here to perform a "sanity check" that all the coverages validate against the schema.
---
+++ test/test_playground_coverages.py
@@ -0,0 +1,27 @@
+# Tests the Coverage objects that are used in the playground, as a sanity check
+
+import os
+from jsonschema import ValidationError
+import pytest
+import json
+
+pytestmark = pytest.mark.schema("/schemas/coveragejson")
+
+def test_all_playground_coverages(validator):
+
+    # Recursively traverse the test_data/playground directory
+    rootpath = os.path.join(os.path.dirname(__file__), "test_data", "playground")
+    for subdir, dirs, files in os.walk(rootpath):
+
+        # Look only at .covjson files
+        for file in (file for file in files if file.endswith(".covjson")):
+
+            # Construct the full file path
+            fullpath = os.path.join(subdir, file)
+
+            # Load the JSON
+            with open(fullpath) as f:
+                j = json.load(f)
+
+            # Validate the JSON
+            validator.validate(j)
